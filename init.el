bdfb129603bbf56c910ddfdbaa063dbd8547be62
Thiago <thiago.calile@gmail.com>
2025-12-05 21:05:11 -0300

    Adiciona dired e mais algumas coisas de Evil Mode

--- init.el
+++ init.el
@@ -15,9 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(all-the-icons counsel doom-modeline evil general ivy ivy-rich
-		   rainbow-delimiters)))
+ '(package-selected-packages nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
