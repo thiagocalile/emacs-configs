f154b73102e5bfae33edc669d2ab7130913000a8
Thiago <thiago.calile@gmail.com>
2025-12-07 14:01:36 -0300

    Conserta um problema com as fontesda barra de modo

--- init.el
+++ init.el
@@ -15,7 +15,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(all-the-icons-dired counsel dired-hide-dotfiles dired-open
+			 doom-modeline evil-collection general
+			 ivy-rich org-roam rainbow-delimiters)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
